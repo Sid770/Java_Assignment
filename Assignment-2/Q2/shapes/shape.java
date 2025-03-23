ed42043d8b22dc515adca74fa44fb0d346450439
Sid770 <166005422+Sid770@users.noreply.github.com>
2025-03-23 12:15:05 +0530

    Create shape.java

---
+++ Assignment-2/Q2/shapes/shape.java
@@ -0,0 +1,8 @@
+// Define the package
+package shapes;
+
+// Define the Shape interface
+public interface Shape {
+    double area();      // Abstract method to calculate area
+    double perimeter(); // Abstract method to calculate perimeter
+}
