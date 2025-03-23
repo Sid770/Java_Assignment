8110f5ae7c2ce9357e359dd718bcb0bfd72437f3
Sid770 <166005422+Sid770@users.noreply.github.com>
2025-03-23 12:12:05 +0530

    Create  MathUtils.java

---
+++ Assignment-2/Q1/utilities/MathUtils.java
@@ -0,0 +1,10 @@
+// Define the package
+package utilities;
+
+// Define the MathUtils class
+public class MathUtils {
+    // Method to add two numbers
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
